a5989b08a8ecdfe729e8c109a2a6a5f192f1a3d9
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-10-29 13:25:44 +0200

    Error handling

--- parcel-transformer-wat/index.js
+++ parcel-transformer-wat/index.js
@@ -1,17 +1,48 @@
 const wabtPromise = require("wabt");
 const path = require("path");
 const { Buffer } = require("buffer");
+const { default: ThrowableDiagnostic, md } = require("@parcel/diagnostic");
 
 const { Transformer } = require("@parcel/plugin");
+
+const ERROR_REGEX = /^parseWat failed:\n[^:]*:(\d):(\d)+: (.*)/;
 
 module.exports = new Transformer({
 	async transform({ asset, logger }) {
 		const wabt = await wabtPromise();
-		const mod = wabt.parseWat(
-			path.basename(asset.filePath),
-			await asset.getCode()
-		);
-		const binary = mod.toBinary({});
+
+		let mod;
+		try {
+			mod = wabt.parseWat(
+				path.basename(asset.filePath),
+				await asset.getCode()
+			);
+		} catch (e) {
+			let match = e.message.match(ERROR_REGEX);
+			if (match != null) {
+				let [, line, column, message] = match;
+				throw new ThrowableDiagnostic({
+					diagnostic: {
+						message: "Failed to parse WAT",
+						codeFrames: [
+							{
+								filePath: asset.filePath,
+								language: "wat",
+								codeHighlights: [
+									{
+										start: { line, column },
+										end: { line, column },
+										message: md`${message}`,
+									},
+								],
+							},
+						],
+					},
+				});
+			}
+			throw e;
+		}
+		let binary = mod.toBinary({});
 
 		asset.type = "wasm";
 		asset.setBuffer(Buffer.from(binary.buffer));
